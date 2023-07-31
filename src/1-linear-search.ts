cb12748936e8529de8b4783e75402f56daaf7c37
Roman Grankov <grankov.r@promail366.com>
2023-08-01 00:06:17 +0300

    добавить линейный поиск

---
+++ src/1-linear-search.ts
@@ -0,0 +1,13 @@
+let count = 0;
+export function linearSearch(array: any[], item: unknown) {
+    for (let i = 0; i < array.length; i++) {
+        count += 1;
+        if (array[i] === item) {
+            console.log('current count = ', count);
+            return i;
+        }
+    }
+
+    console.log('max count = ', count);
+    return null;
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { linearSearch } from './1-linear-search';
+
+const array = [1, 4, 5, 8, 5, 1, 2, 7, 5, 2, 11];
+console.log(linearSearch(array, 111)); // count = 11 - длине массива
